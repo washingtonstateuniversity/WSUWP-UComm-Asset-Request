0f0cdbe42f7e3f9e5a711d3d1aee22ba37bd1f1a
Charles J Dillon <charles.dillon2@wsu.edu>
2014-02-27 11:14:19 -0800

    CSS adjustments and modified header
    Removed unnecessary css.
    Added new header styles
    Adjusted the hr tag to be a divider
    Created a page footer class to be applied to sections at the bottom of
    the page
    ul and li adjustments
    block quote adjustments

--- wp-content/themes/ucomm/parts/headers.php
+++ wp-content/themes/ucomm/parts/headers.php
@@ -2,27 +2,35 @@
 
 	global $post;
 	
-	$site_name = get_bloginfo('name');
-	$site_tagline = get_bloginfo('description');
+	$site_name      = get_bloginfo('name');
+	$site_tagline   = get_bloginfo('description');
 	$first_category = get_the_category();
-	$section_title = get_the_category();
-	
-	$words = explode(' ', $site_name);
-	$words[1] = '<span class="addGray">'.$words[1].'</span>';
-	$pretty_name = implode(' ', $words);
-
- 
-	echo '<header>';
-	echo '<hgroup>';
-	echo '	<div class="site"><a href="'.home_url().'" title="'.$site_name.'" rel="home">'.$pretty_name.'</a></div>';
-	echo '	<div class="tagline"><a href="'.home_url().'" title="'.$site_tagline.'" rel="home">'.$site_tagline.'</a></div>';
-	if (is_subpage()) { echo '	<div class="section">'.section_title().'</div>'; }
-	if (is_category() || is_single() || is_archive() ) { echo '	<div class="category">'.$first_category[0]->cat_name.'</div>'; }
-	if (is_page()) { echo '	<div class="page">'.get_the_title().'</div>'; }
-	if (is_single()) { echo '	<div class="post">'.get_the_title().'</div>'; }
-	echo '</hgroup>';
-	echo '</header>';
-	
-	
-	
+	$section_title  = get_the_category();
 ?>
+
+<header>
+<hgroup>
+	<!-- Removed ?php echo esc_html( $site_name ); ?> and placed University <span class="addGray">Communications</span> to achieve split word colors -->
+	<div class="site"><a href="<?php home_url(); ?>" title="<?php echo esc_attr( $site_name ); ?>" rel="home">University <span class="addGray">Communications</span></a></div>
+	<div class="tagline"><a href="<?php home_url(); ?>" title="<?php echo esc_attr( $site_tagline ); ?>" rel="home"><?php echo esc_html( $site_tagline ); ?></a></div>
+	<?php
+
+	if ( spine_is_subpage() ) {
+		echo '	<div class="section">' . spine_section_title() . '</div>';
+	}
+
+	if ( ( is_archive() || is_category() || is_single() ) && ! empty( $first_category ) && isset( $first_category[0]->cat_name ) ) {
+		echo '	<div class="category">'.$first_category[0]->cat_name.'</div>';
+	}
+
+	if ( is_page() ) {
+		echo '	<div class="page">' . get_the_title() . '</div>';
+	}
+
+	if ( is_single() ) {
+		echo '	<div class="post">' . get_the_title() . '</div>';
+	}
+
+	?>
+</hgroup>
+</header>

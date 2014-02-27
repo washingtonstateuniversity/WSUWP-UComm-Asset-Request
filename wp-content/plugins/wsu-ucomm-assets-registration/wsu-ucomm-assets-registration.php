22d102234d5e398283baca638406ec4a1454c52f
jeremyfelt <jeremy.felt@gmail.com>
2014-02-27 11:26:47 -0800

    Register a post type to handle asset registration requests
    This post type will become an interface through which approvals can be made to allow users access to various assets.

--- wp-content/plugins/wsu-ucomm-assets-registration/wsu-ucomm-assets-registration.php
+++ wp-content/plugins/wsu-ucomm-assets-registration/wsu-ucomm-assets-registration.php
@@ -12,12 +12,18 @@ License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2
 class WSU_UComm_Assets_Registration {
 
 	/**
+	 * @var string Post type slug for asset requests.
+	 */
+	var $post_type_slug = 'ucomm_asset_request';
+
+	/**
 	 * Setup the hooks.
 	 */
 	public function __construct() {
 		add_filter( 'wsuwp_sso_create_new_user', array( $this, 'wsuwp_sso_create_new_user' ) );
 		add_filter( 'wsuwp_sso_new_user_role',   array( $this, 'wsuwp_sso_new_user_role'   ) );
 		add_action( 'wsuwp_sso_user_created',    array( $this, 'remove_user_roles'         ) );
+		add_action( 'init',                      array( $this, 'register_post_type'        ) );
 	}
 
 	/**
@@ -47,6 +53,43 @@ class WSU_UComm_Assets_Registration {
 	public function remove_user_roles( $user_id ) {
 		$user = get_userdata( $user_id );
 		$user->set_role( '' );
+	}
+
+	/**
+	 * Register the post type used to handle asset registration requests.
+	 */
+	public function register_post_type() {
+		$labels = array(
+			'name'               => 'Asset Request',
+			'singular_name'      => 'Asset Request',
+			'add_new'            => 'Add New',
+			'add_new_item'       => 'Add New Asset Request',
+			'edit_item'          => 'Edit Asset Request',
+			'new_item'           => 'New Asset Request',
+			'all_items'          => 'All Asset Requests',
+			'view_item'          => 'View Asset Request',
+			'search_items'       => 'Search Asset Requests',
+			'not_found'          => 'No asset requests found',
+			'not_found_in_trash' => 'No asset requests found in Trash',
+			'parent_item_colon'  => '',
+			'menu_name'          => 'Asset Requests',
+		);
+
+		$args = array(
+			'labels'             => $labels,
+			'public'             => true,
+			'publicly_queryable' => false,
+			'show_ui'            => true,
+			'show_in_menu'       => true,
+			'show_in_admin_bar'  => false,
+			'query_var'          => true,
+			'capability_type'    => 'post',
+			'hierarchical'       => false,
+			'menu_position'      => 5,
+			'supports'           => array( 'title', 'editor' ),
+		);
+
+		register_post_type( $this->post_type_slug, $args );
 	}
 }
 new WSU_UComm_Assets_Registration();
